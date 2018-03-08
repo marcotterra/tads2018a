6df3ba8c2a30c4c7ec7ed6e56c4ff289ed438c69
marcotterra <marcotulio.mtt@gmail.com>
2018-03-08 00:59:41 +0000

    Desafio 2 concluido

--- 02_lista_convidados/scripts.js
+++ 02_lista_convidados/scripts.js
@@ -1,6 +1,10 @@
 class List {
     constructor() {
         this.arrInvited = [];
+        this.childrens = 0;
+        this.teenagers = 0;
+        this.adults = 0;
+        this.olds = 0;
     }
 
 
@@ -13,6 +17,55 @@ class List {
         person.age = age;
 
         this.arrInvited.push(person);
+    }
+
+    showli(strings, ...values) {
+        return `<li>
+                    <span class="box-item left text-center">${strings.name}</span>
+                    <span class="box-item right text-center">${strings.age}</span>
+                </li>`
+    }
+
+    show() {
+
+        let table1 = document.getElementById('ul-table1');
+        let arrar = this.arrInvited;
+        table1.innerHTML = '';
+
+        for (let i = 0; i < arrar.length; i++) {
+            table1.innerHTML += this.showli(arrar[i]);
+        }
+
+    }
+
+    cReports() {
+
+        for (let i = 0; i < this.arrInvited.length; i++) {
+            if (this.arrInvited[i].age < 13) {
+                this.childrens++;
+            } else if (this.arrInvited[i].age < 18) {
+                this.teenagers++;
+            } else if (this.arrInvited[i].age < 56) {
+                this.adults++;
+            } else {
+                this.olds++;
+            }
+        }
+    }
+
+    sReports(){
+
+        this.cReports();
+
+        let childrens = document.getElementById ("child");
+        let teenagers = document.getElementById ("teenagers");
+        let adults = document.getElementById ("adults");
+        let olds = document.getElementById ("olds");
+        
+        childrens.innerHTML = this.childrens;
+        teenagers.innerHTML = this.teenagers;
+        adults.innerHTML = this.adults;
+        olds.innerHTML = this.olds;
     }
 }
 
